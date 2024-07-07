67b7b6f199b58a3487152afebad2ddaabee10a0e
PonPechraksmey0 <139233349+PonPechraksmey@users.noreply.github.com>
2024-07-07 13:28:39 +0700

    creat page detail

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -3,12 +3,50 @@
 </script>
 
 <template>
-    <div>
-        <h1>Details pages</h1>
-    </div>
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex justify-center w-5/6 h-[800px] mt-8 border-2 border-gray-300">
+            <div class="flex flex-col w-[1000px] h-[800px] bg-slate-400">
+                <div>
+                    <p class="p-8">ដំណើរទស្សនកិច្ចសិក្សាទៅកាន់ខេត្តមណ្ឌលគិរី ក្នុងដំណើរការ ទស្សនកិច្ចនេះនិសិ្សតបានចុះកិច្ចសិក្សានិងប្រមូលព័ត៍មាន
+                        ដំណើរទស្សនកិច្ចសិក្សាទៅកាន់ខេត្តមណ្ឌលគិរី ក្នុងដំណើរការ ទស្សនកិច្ចនេះ និសិ្សតបានចុះកិច្ចសិក្សា និងប្រមូលព័ត៍មាន...</p>
+                </div>
+                <div class="flex justify-start items-center">
+                    <div>
+                        <Icon class="size-[32px]" icon="mingcute:notification-fill"/>
+                    </div>
+                    <div>
+                        <p class="px-8">ផុសថ្ងៃ: 12-01-2024</p>
+                    </div>
+                </div>
+                <div class="<style>
+                w=
+                </style>">
+                    <img src="/public/images/content/Rectangle 43.png" alt="">
+                </div>
+            </div>
+            <div class="flex w-[550px] h-[800px] bg-slate-400 ml-1">
+
+            </div>
+        </div>
+        </div>
 </template>
 
 
 <style scoped>
-
+   h1{
+          font-family: 'Khmer OS Muol';
+          font-style: normal;
+          font-weight: 300;
+          font-size: xx-large;
+      }
+      p{
+          font-family: 'Khmer OS Battambang';
+          font-style: normal;
+          font-weight: normal;
+          font-size: xx-large;
+      }
+      button{
+        font-family: Khmer OS Battambang;
+        background-color: rgb(98, 149, 162);
+      }
 </style>
